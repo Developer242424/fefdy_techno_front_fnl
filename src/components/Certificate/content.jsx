73b0e2a8a82ac52840b9517bd7b4da4496034081
mohamednoordeen2915 <asik29072000@gmail.com>
2025-07-08 11:08:56 +0530

    certficate content changes

--- src/components/Certificate/content.jsx
+++ src/components/Certificate/content.jsx
@@ -7,49 +7,50 @@ import Iao from "../../defaults/img/iao.png";
 import Signature from "../../defaults/img/signature.png";
 
 const Certificate = () => {
-  return (
-    <div class="certificate">
-      <div class="inner-border">
-        <img src={Corner} class="corner tl" />
-        <img src={Corner} class="corner tr" />
-        <img src={Corner} class="corner bl" />
-        <img src={Corner} class="corner br" />
+    return (
+        <div class="certificate">
+            <div class="inner-border">
+                <img src={Corner} class="corner tl" />
+                <img src={Corner} class="corner tr" />
+                <img src={Corner} class="corner bl" />
+                <img src={Corner} class="corner br" />
 
-        <div class="logo">
-          <img src={FefdyLogo} class="logo-main" alt="FEFDY Logo" />
-        </div>
-        <div class="certificate-header">
-          <img src={Ciaa} class="logo_1" alt="Left Logo" />
-          <div class="title-text">
-            <h1 class="certificate-title">CERTIFICATE</h1>
-            <h2 class="certificate-subtitle">OF EXCELLENCE</h2>
-          </div>
-          <img src={Iao} class="logo_1" alt="Right Logo" />
-        </div>
-        <p class="presented">THIS CERTIFICATE IS PROUDLY PRESENTED TO</p>
-        <p class="description">
-          <span class="recipient-inline">Thanikachalam Venkataramanan</span>,
-          Age:
-          <span class="recipient-inline">24</span>, for successfully completing
-          all 8 levels in Science, Math and English in the{" "}
-          <strong> FEFDY Brain Gym</strong>, demonstrating consistent excellence
-          throughout the year <span class="recipient-inline">2025</span>.
-        </p>
+                <div class="logo">
+                    <img src={FefdyLogo} class="logo-main" alt="FEFDY Logo" />
+                </div>
+                <div class="certificate-header">
+                    <img src={Ciaa} class="logo_1" alt="Left Logo" />
+                    <div class="title-text">
+                        <h1 class="certificate-title">CERTIFICATE</h1>
+                        <h2 class="certificate-subtitle">OF EXCELLENCE</h2>
+                    </div>
+                    <img src={Iao} class="logo_1" alt="Right Logo" />
+                </div>
+                <p class="presented">THIS CERTIFICATE IS PROUDLY PRESENTED TO</p>
+                <p class="description">
+                    <span class="recipient-inline">Thanikachalam Venkataramanan</span>,
+                    Age:
+                    <span class="recipient-inline">24</span>, for completing the following Subject in the <strong>FEFDY Brain Gym</strong>  with dedication:  <span className="subject_c">Science -</span>  <span className="chapter_c">C1</span>  <span className="levels_c">(L1, L2, L3), </span> <span className="chapter_c">C2</span>  <span className="levels_c">(L1, L2), </span>
+                    <span className="subject_c">Math -</span>  <span className="chapter_c">C1</span>  <span className="levels_c">(L1, L2, L3), </span> <span className="chapter_c">C2</span>  <span className="levels_c">(L1, L2), </span>
+                    <span className="subject_c">English -</span>  <span className="chapter_c">C1</span>  <span className="levels_c">(L1, L2, L3), </span> <span className="chapter_c">C2</span>  <span className="levels_c">(L1, L2), </span>
 
-        <div class="footer_certificate">
-          <div class="footer-box">
-            <div class="footer-line"></div>
-            <span>DATE</span>
-          </div>
-          <div class="footer-box">
-            <img src={Signature} class="signature" />
-            <div class="footer-line"></div>
-            <span>FOUNDER OF FEFDY</span>
-          </div>
+                    This achievement is recognized and appreciated throughout the year <span class="recipient-inline">2025</span>.
+                </p>
+
+                <div class="footer_certificate">
+                    <div class="footer-box">
+                        <div class="footer-line"></div>
+                        <span>DATE</span>
+                    </div>
+                    <div class="footer-box">
+                        <img src={Signature} class="signature" />
+                        <div class="footer-line"></div>
+                        <span>FOUNDER OF FEFDY</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Certificate;
